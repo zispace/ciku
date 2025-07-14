06bee9ef6e080fcfe783714b44526503df228b4c
hantang <34468720+hantang@users.noreply.github.com>
2025-07-14 16:55:55 +0800

    Update config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // 启用静态导出
+  basePath: '/ciku',         // 二级目录
+  assetPrefix: '/ciku/',     // 静态资源前缀
 };
 
 export default nextConfig;
